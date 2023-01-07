60322e140ff7ac74fa5012a922685ee7724f7235
Abdullah662000 <abdullahzafar11223344@gmail.com>
2023-01-07 11:55:34 +0500

    updated product schema

--- model/Product.js
+++ model/Product.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const schema = mongoose.Schema({
   name: { type: String, required: true },
+  branchId: { type: mongoose.Types.ObjectId, required: true },
   image: { data: Buffer, contentType: String },
   orignalPrice: { type: Number, required: true },
   offerPrice: Number,
