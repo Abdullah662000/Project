0bd1bd1a79c9eb6f80a2ed76cbb3e9be2b12906f
Abdullah662000 <abdullahzafar11223344@gmail.com>
2023-01-12 17:12:12 +0500

    a

--- controller/verification.js
+++ controller/verification.js
@@ -1,9 +1,12 @@
 const jwt = require("jsonwebtoken");
 module.exports = async function (req, res, next) {
   const t = await req.headers["authorization"];
-  console.log(t);
-  const bearer = await t.split(" ");
-  const token = bearer[1];
+  if (t) {
+    const bearer = await t.split(" ");
+    const token = bearer[1];
+  } else {
+    return res.status(400).json("token not found");
+  }
   if (!token) {
     return res.status(400).json("token not found");
   }
